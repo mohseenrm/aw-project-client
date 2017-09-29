aaceb41bf87bfa7784f08b23a898b5e2b699bafa
mohseenrm <mohseenmukaddam6@gmail.com>
2017-09-28 22:23:20 -0700

    updates

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import * as ReactDOM from 'react-dom';
 import { App } from './components/App';
 
 ReactDOM.render (
-    <App 
-			compiler="TypeScript" 
-			framework="React"
-		/>,
-    document.getElementById ( 'app' ),
+	<App
+		compiler="TypeScript"
+		framework="React"
+	/>,
+	document.getElementById ( 'app' ),
 );
