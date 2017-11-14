a1444342c854bc0585bd0281a674b1ef2dc6b79e
mohseenrm <mohseenmukaddam6@gmail.com>
2017-11-13 17:51:57 -0700

    Refactor and formatting modal + card

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,8 +4,8 @@ import {
   Card,
   Header,
   Icon,
-   Image,
-   Modal,
+  Image,
+  Modal,
 } from 'semantic-ui-react';
 
 interface CardProps {
@@ -15,22 +15,26 @@ interface CardProps {
 export default class Cards extends React.Component < CardProps, any > {
   constructor (props: any) {
     super(props);
-    this.state = {clicked:false};
-    this.handleClick=this.handleClick.bind(this);
+    this.state = { clicked:false };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   // onclick function of card
- handleClick(){
-
-        this.setState({clicked: !this.state.clicked});
-        console.log(this.state);
-
-
+  handleClick () {
+    // this pattern is used to copy keys from previous state and update new ones
+    this.setState(
+      Object.assign(
+        {},
+        this.state,
+        { clicked: !this.state.clicked },
+      ),
+    );
   }
 
   render () {
     return(
 			<div className="main-wrapper--cards">
+        {/* Card */}
 				<Card.Group basic={true} size="small" itemsPerRow="three">
           <Card
             fluid={true}
@@ -72,27 +76,32 @@ export default class Cards extends React.Component < CardProps, any > {
             </Card.Content>
           </Card>
         </Card.Group>
-
+        {/* Modal */}
         <Modal open={this.state.clicked} >
-    <Modal.Header>Profile Picture</Modal.Header>
-    <Modal.Content image scrolling>
-      <Image
-        size='medium'
-        src='/assets/images/wireframe/image.png'
-        wrapped
-      />
-
-      <Modal.Description>
-        <Header>Modal Header</Header>
-        <p>This is an example of expanded content that will cause the modal's dimmer to scroll</p>
-      </Modal.Description>
-    </Modal.Content>
-    <Modal.Actions>
-      <Button primary onClick={this.handleClick}>
-        Proceed <Icon name="chevron right" />
-      </Button>
-    </Modal.Actions>
-  </Modal>
+          <Modal.Header>Profile Picture</Modal.Header>
+          <Modal.Content
+            image={true}
+            scrolling={true}
+          >
+            <Image
+              size="medium"
+              src="/assets/images/wireframe/image.png"
+              wrapped={true}
+            />
+            <Modal.Description>
+              <Header>Modal Header</Header>
+              <p>This is an example of expanded content that will cause the modal's dimmer to scroll</p>
+            </Modal.Description>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button
+              primary={true}
+              onClick={this.handleClick}
+            >
+              Proceed <Icon name="chevron right" />
+            </Button>
+          </Modal.Actions>
+        </Modal>
 			</div>
     );
   }
