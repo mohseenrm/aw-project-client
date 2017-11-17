8c448d8c04e73acfdbc5ba3c408ad7e5a28697ea
mohseenrm <mohseenmukaddam6@gmail.com>
2017-11-17 00:38:54 -0700

    hooking up form controls

--- src/components/CreateUser.tsx
+++ src/components/CreateUser.tsx
@@ -1,9 +1,9 @@
+import axios from 'axios';
 import * as React from 'react';
+
 import {
 	Button,
-	Divider,
 	Form,
-	Icon,
 	Input,
  } from 'semantic-ui-react';
 
@@ -12,14 +12,42 @@ interface CreateUserProps {
 }
 
 interface CreateUserState {
+	first_name: string;
+	last_name: string;
+	user_name: string;
+	password: string;
 }
 
 export default class CreateUser extends React.Component < CreateUserProps, CreateUserState > {
   constructor (props: any) {
     super(props);
-		  this.state = {};
-		  this.backToLogin = this.backToLogin.bind(this);
-  }
+		this.state = {
+			first_name: '',
+			last_name: '',
+			user_name: '',
+			password: '',
+		};
+
+		this.backToLogin = this.backToLogin.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange (e: any, state: any) {
+		console.log(e.target);
+		const { name, value } = e.target;
+
+		this.setState(
+			Object.assign(
+				{},
+				this.state,
+				{ [name]: value },
+			),
+		);
+	}
+	
+	sendRequest (event: any) {
+		console.log(this.state);
+	}
 
   backToLogin (event: any) {
     this.props.callbackParent(false);
@@ -28,16 +56,20 @@ export default class CreateUser extends React.Component < CreateUserProps, Creat
   render () {
     return(
 			<div className="main-wrapper--create-user">
-				<Form size="big">
+				<Form size="big" onSubmit={this.sendRequest}>
 					<Form.Group unstackable={true} widths="equal">
 						<Form.Field
 							label="First name"
+							name="first_name"
+							onChange={this.handleChange}
 							control="input"
 							placeholder="First name"
 							width={4}
 						/>
 						<Form.Field
 							label="Last name"
+							name="last_name"
+							onChange={this.handleChange}
 							control="input"
 							placeholder="Last name"
 							width={4}
@@ -46,24 +78,28 @@ export default class CreateUser extends React.Component < CreateUserProps, Creat
 					<Form.Group unstackable={true} widths="equal">
 						<Form.Field
 							label="Username"
+							name="user_name"
+							onChange={this.handleChange}
 							control="input"
 							placeholder="john_bohnam"
 							width={4}
 						/>
 						<Form.Field
 							label="Password"
+							name="password"
+							onChange={this.handleChange}
 							control="input"
 							placeholder="password"
 							width={4}
 						/>
 					</Form.Group>
 					<div className="main-wrapper--create-user--buttons">
-						<Button
+						<Form.Button
 							primary={true}
 							size="huge"
 						>
 							Submit
-						</Button>
+						</Form.Button>
 						<Button
 							primary={false}
 							size="huge"
