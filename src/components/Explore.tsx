37af4bc706e19d82204d32007746d7c6ba50ca71
mohseenrm <mohseenmukaddam6@gmail.com>
2017-11-24 19:15:54 -0700

    updating explore component with options

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -9,6 +9,7 @@ interface ExploreProps {
 }
 
 interface ExploreState {
+  type: string;
 }
 
 const options = [
@@ -18,33 +19,53 @@ const options = [
 
 export default class Explore extends React.Component < ExploreProps, ExploreState > {
   constructor (props: any) {
-    super(props);
-    this.state = {};
+		/* tslint:disable */
+		super(props);
+		this.state = {
+			type: 'all',
+		};
+		this.selectTypeOfSearch = this.selectTypeOfSearch.bind(this);
+		/* tslint:enable */
+  }
+
+  selectTypeOfSearch (data: any, element: any) {
+    this.setState(
+			Object.assign(
+				{},
+				this.state,
+				{ type: element.value },
+			),
+		);
   }
 
   render () {
     return(
-			<div>
-				<Input
-					type="text"
-					placeholder="Search..."
-					action={true}
-					size="huge"
-				>
-					<input />
-					<Select
-						compact={true}
-						options={options}
-						defaultValue="all"
-						size="huge"
-					/>
-					<Button
-						type="submit"
+			<div className="search-wrapper">
+				<div className="search-wrapper--search">
+					<Input
+						type="text"
+						placeholder="Search..."
+						action={true}
 						size="huge"
 					>
-						Search
-					</Button>
-				</Input>
+						<input />
+						<Select
+							compact={true}
+							options={options}
+							onChange={this.selectTypeOfSearch}
+							defaultValue="all"
+							size="huge"
+						/>
+						<Button
+							color="blue"
+							primary={true}
+							type="submit"
+							size="huge"
+						>
+							Search
+						</Button>
+					</Input>
+				</div>
 			</div>
     );
   }
