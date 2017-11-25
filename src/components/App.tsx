576022c6261d781818683b3a16e31bed9295ce94
mohseenrm <mohseenmukaddam6@gmail.com>
2017-11-24 17:53:48 -0700

    adding tab

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Tab } from 'semantic-ui-react';
 
 import Profile from './Profile';
 import Login from './Login';
@@ -78,13 +79,39 @@ export default class App extends React.Component < AppProps, AppState > {
         />
       );
     } else if (this.state.auth) {
+      const panes = [
+        {
+          menuItem: {
+            content: 'Users',
+            icon: 'users',
+            inverted: true,
+            key: 'users',
+          },
+          render: () => {
+            return(
+              <div className="profile-wrapper">
+                <Profile
+                  token={this.state.token}
+                  username={this.state.username}
+                />
+              </div>
+            );
+          },
+        },
+        {
+          menuItem: 'Tab 2',
+          render: () => <Tab.Pane>Tab 2 Content</Tab.Pane>,
+        },
+        {
+          menuItem: 'Tab 3',
+          render: () => <Tab.Pane>Tab 3 Content</Tab.Pane>,
+        },
+      ];
+
       return(
-        <div className="profile-wrapper">
-          <Profile
-            token={this.state.token}
-            username={this.state.username}
-          />
-        </div>
+        <Tab
+          panes={panes}
+        />
       );
     } else {
       return(
